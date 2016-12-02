364cb44996be85deaf69485b399156f6d86b4d05
rmainhar <rmainhar@shakopee.k12.mn.us>
2016-12-02 11:13:02 -0600

    Finished the code
    I am done

--- team0.py
+++ team0.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'MAINHARDT' # Only 10 chars displayed.
+strategy_name = 'ALWAYS COLLUDE'
+strategy_description = 'I am lazy and dont cafe about this assingment please fail me'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
